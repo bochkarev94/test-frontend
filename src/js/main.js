86deb40861d41ab8cc7fdf35f9f0974855d53940
Ivan <akbars06_2010@mail>
2021-01-29 17:04:41 +0300

    add hamburger

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,10 @@
+import hamburger from './modules/hamburger';
 import modalMenu from './modules/modalMenu';
 
 window.addEventListener('DOMContentLoaded', () => {
 'use strict';
 
+hamburger('.header__hamburger', '#header-menu', '.overlay__tp', '.menu__close');
 modalMenu('[data-menu="long"]', '#long', '.overlay', 'grid');
 modalMenu('[data-menu="short"]', '#short', '.overlay', 'grid');
 })
---
+++ src/js/modules/hamburger.js
@@ -0,0 +1,20 @@
+
+const hamburger = (trigger, idSelector, overlaySelector, closeMenu) => {
+
+    const hamburger = document.querySelector(trigger),
+        menu = document.querySelector(idSelector),
+        overlay = document.querySelector(overlaySelector),
+        close = document.querySelector(closeMenu);
+
+        hamburger.addEventListener('click', () => {
+            overlay.style.display = 'block';
+            menu.style.marginLeft = `0px`;
+            document.body.style.overflow = 'hidden';
+        });
+        close.addEventListener('click', () => {
+            overlay.style.display = 'none';
+            menu.style.marginLeft = `-400px`;
+            document.body.style.overflow = '';
+        });
+}
+export default hamburger;
